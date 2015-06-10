c822f608c2cc1302836edf5876fc4234bff6ab09
Vinnie Padmanabhan <vinnie329@gmail.com>
2015-06-10 23:53:21 +0100

    phoenix wrapper w mobile font-size change

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -184,6 +184,15 @@ var colourInterval = setInterval(function(){
 	}
 }, 200);
 
+if( navigator.userAgent.match(/Android/i)
+                || navigator.userAgent.match(/webOS/i)
+                || navigator.userAgent.match(/iPhone/i)
+                || navigator.userAgent.match(/iPad/i)
+                || navigator.userAgent.match(/iPod/i)
+                || navigator.userAgent.match(/BlackBerry/i)
+                || navigator.userAgent.match(/Windows Phone/i)
+            ){document.getElementsByClassName("tagLine")[0].className+="tagLineMobile";}
+
 $(document).ready(function(){
 
 	document.getElementById('player').style.visibility = "hidden";
