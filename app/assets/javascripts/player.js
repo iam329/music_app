68b0f064d7f1bf1ad520e626425510b14939de57
Vinnie Padmanabhan <vinnie329@gmail.com>
2015-06-10 23:56:29 +0100

    phoenix wrapper w overflow rules

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -184,15 +184,6 @@ var colourInterval = setInterval(function(){
 	}
 }, 200);
 
-if( navigator.userAgent.match(/Android/i)
-                || navigator.userAgent.match(/webOS/i)
-                || navigator.userAgent.match(/iPhone/i)
-                || navigator.userAgent.match(/iPad/i)
-                || navigator.userAgent.match(/iPod/i)
-                || navigator.userAgent.match(/BlackBerry/i)
-                || navigator.userAgent.match(/Windows Phone/i)
-            ){document.getElementsByClassName("tagLine")[0].className+="tagLineMobile";}
-
 $(document).ready(function(){
 
 	document.getElementById('player').style.visibility = "hidden";
